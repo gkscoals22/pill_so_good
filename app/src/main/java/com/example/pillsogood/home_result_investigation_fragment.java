4c1117c1194ca5abc187c98683bc8c04a06dc447
cham <gkscoals1@gmail.com>
2020-06-18 02:23:16 +0900

    Add dialog about survey.

--- app/src/main/java/com/example/pillsogood/home_result_investigation_fragment.java
+++ app/src/main/java/com/example/pillsogood/home_result_investigation_fragment.java
@@ -1,7 +1,12 @@
 package com.example.pillsogood;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,6 +20,11 @@ public class home_result_investigation_fragment extends Fragment {
     public static home_result_investigation_fragment newInstance() {
         return new home_result_investigation_fragment();
     }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_result_investigation, container, false);
 
-
+        return view;
+    }
 }
--- app/src/main/java/com/example/pillsogood/home_selected_investigation_fragment.java
+++ app/src/main/java/com/example/pillsogood/home_selected_investigation_fragment.java
@@ -1,5 +1,6 @@
 package com.example.pillsogood;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 public class home_selected_investigation_fragment extends Fragment {
@@ -22,6 +24,16 @@ public class home_selected_investigation_fragment extends Fragment {
     private static int vitaminD = 0;
     private static int omega3 = 0;
     private static int lactobacillus = 0;
+
+    private static boolean isChecked10 = false;
+    private static boolean isChecked11 = false;
+    private static boolean isChecked12 = false;
+    private static boolean isChecked13 = false;
+    private static boolean isChecked14 = false;
+    private static boolean isChecked15 = false;
+    private static boolean isChecked16 = false;
+    private static boolean isChecked17 = false;
+
     private Button resultButton;
 
     private CheckBox chk10_1;
@@ -73,55 +85,6 @@ public class home_selected_investigation_fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         view = inflater.inflate(R.layout.home_selected_investigation,container,false);
 
-        //다른 클래스 정의
-        home_question_investigation_fragment surveyListClass = new home_question_investigation_fragment();
-
-        //Linearlayout id 가져오기
-        LinearLayout layout1 = (LinearLayout)view.findViewById(R.id.layout_eye);
-        LinearLayout layout2 = (LinearLayout)view.findViewById(R.id.layout_brain);
-        LinearLayout layout3 = (LinearLayout)view.findViewById(R.id.layout_tired);
-        LinearLayout layout4 = (LinearLayout)view.findViewById(R.id.layout_bone);
-        LinearLayout layout5 = (LinearLayout)view.findViewById(R.id.layout_immune);
-        LinearLayout layout6 = (LinearLayout)view.findViewById(R.id.layout_hair);
-        LinearLayout layout7 = (LinearLayout)view.findViewById(R.id.layout_rotation);
-        LinearLayout layout8 = (LinearLayout)view.findViewById(R.id.layout_digestion);
-        LinearLayout layout9 = (LinearLayout)view.findViewById(R.id.layout_skin);
-
-        //button id 가져오기
-        resultButton = view.findViewById(R.id.result_btn);
-
-        //이미지 선택되지 않았을 때 내용 보이지 않게 처리
-        if(!surveyListClass.getSelectImage1())
-            layout1.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage2())
-            layout2.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage3())
-            layout3.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage4())
-            layout4.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage5())
-            layout5.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage6())
-            layout6.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage7())
-            layout7.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage8())
-            layout8.setVisibility(View.GONE);
-        if(!surveyListClass.getSelectImage9())
-            layout9.setVisibility(View.GONE);
-
-//        selectCheckSurvey();
-        resultButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                selectCheckSurvey();
-                MainActivity activity = (MainActivity)getActivity();
-                activity.replaceFragment(home_result_investigation_fragment.newInstance());
-            }
-        });
-        return view;
-    }
-
-    void selectCheckSurvey(){
         chk10_1 = (CheckBox)view.findViewById(R.id.check10_1);
         chk10_2 = (CheckBox)view.findViewById(R.id.check10_2);
         chk10_3 = (CheckBox)view.findViewById(R.id.check10_3);
@@ -164,6 +127,70 @@ public class home_selected_investigation_fragment extends Fragment {
         chk17_3 = (CheckBox)view.findViewById(R.id.check17_3);
         chk17_4 = (CheckBox)view.findViewById(R.id.check17_4);
 
+        //다른 클래스 정의
+        home_question_investigation_fragment surveyListClass = new home_question_investigation_fragment();
+
+        //Linearlayout id 가져오기
+        LinearLayout layout1 = (LinearLayout)view.findViewById(R.id.layout_eye);
+        LinearLayout layout2 = (LinearLayout)view.findViewById(R.id.layout_brain);
+        LinearLayout layout3 = (LinearLayout)view.findViewById(R.id.layout_tired);
+        LinearLayout layout4 = (LinearLayout)view.findViewById(R.id.layout_bone);
+        LinearLayout layout5 = (LinearLayout)view.findViewById(R.id.layout_immune);
+        LinearLayout layout6 = (LinearLayout)view.findViewById(R.id.layout_hair);
+        LinearLayout layout7 = (LinearLayout)view.findViewById(R.id.layout_rotation);
+        LinearLayout layout8 = (LinearLayout)view.findViewById(R.id.layout_digestion);
+        LinearLayout layout9 = (LinearLayout)view.findViewById(R.id.layout_skin);
+
+        //button id 가져오기
+        resultButton = view.findViewById(R.id.result_btn);
+
+        //이미지 선택되지 않았을 때 내용 보이지 않게 처리
+        if(!surveyListClass.getSelectImage1())
+            layout1.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage2())
+            layout2.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage3())
+            layout3.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage4())
+            layout4.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage5())
+            layout5.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage6())
+            layout6.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage7())
+            layout7.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage8())
+            layout8.setVisibility(View.GONE);
+        if(!surveyListClass.getSelectImage9())
+            layout9.setVisibility(View.GONE);
+
+//        selectCheckSurvey();
+        resultButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                initialize();
+                selectCheckSurvey();
+                MainActivity activity = (MainActivity)getActivity();
+                if(isAllCheck()){
+                    activity.replaceFragment(home_result_investigation_fragment.newInstance());
+                }
+                else{
+                    AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+                    alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();     //닫기
+                        }
+                    });
+                    alert.setMessage("체크 하지 않은 항목이 있습니다.");
+                    alert.show();
+
+                }
+            }
+        });
+        return view;
+    }
+
+    void selectCheckSurvey(){
         //10번 질문
         if(chk10_3.isChecked()){
             vitaminB++;
@@ -248,6 +275,39 @@ public class home_selected_investigation_fragment extends Fragment {
         }
     }
 
+    public boolean isAllCheck(){
+        if(chk10_1.isChecked() || chk10_2.isChecked() || chk10_3.isChecked()){
+            isChecked10 = true;
+        }
+        if(chk11_1.isChecked() || chk11_2.isChecked() || chk11_3.isChecked() || chk11_4.isChecked() || chk11_5.isChecked()){
+            isChecked11 = true;
+        }
+        if(chk12_1.isChecked() || chk12_2.isChecked() || chk12_3.isChecked()){
+            isChecked12 = true;
+        }
+        if(chk13_1.isChecked() || chk13_2.isChecked() || chk13_3.isChecked() || chk13_4.isChecked() || chk13_5.isChecked() || chk13_6.isChecked()){
+            isChecked13 = true;
+        }
+        if(chk14_1.isChecked() || chk14_2.isChecked() || chk14_3.isChecked() || chk14_4.isChecked() || chk14_5.isChecked()){
+            isChecked14 = true;
+        }
+        if(chk15_1.isChecked() || chk15_2.isChecked() || chk15_3.isChecked() || chk15_4.isChecked() || chk15_5.isChecked()){
+            isChecked15 = true;
+        }
+        if(chk16_1.isChecked() || chk16_2.isChecked() || chk16_3.isChecked()){
+            isChecked16 = true;
+        }
+        if(chk17_1.isChecked() || chk17_2.isChecked() || chk17_3.isChecked() || chk17_4.isChecked()){
+            isChecked17 = true;
+        }
+
+        if(isChecked10 && isChecked11 && isChecked12 && isChecked13 && isChecked14 && isChecked15 && isChecked16 && isChecked17){
+            return true;
+        }
+        else
+            return false;
+    }
+
     public static int getLactobacillus() {
         return lactobacillus;
     }
@@ -295,5 +355,24 @@ public class home_selected_investigation_fragment extends Fragment {
     }
     public static void setVitaminD(int vitaminD) {
         home_selected_investigation_fragment.vitaminD = vitaminD;
+    }
+    public void initialize(){
+        lutein = 0;
+        magnesium = 0;
+        vitaminA = 0;
+        vitaminB = 0;
+        vitaminC = 0;
+        vitaminD = 0;
+        omega3 = 0;
+        lactobacillus = 0;
+
+        isChecked10 = false;
+        isChecked11 = false;
+        isChecked12 = false;
+        isChecked13 = false;
+        isChecked14 = false;
+        isChecked15 = false;
+        isChecked16 = false;
+        isChecked17 = false;
     }
 }
